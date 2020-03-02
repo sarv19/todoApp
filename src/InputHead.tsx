259243f4c882b87118bc9af4b1d2d986f203f004
unknown <vongocquynhnhu@gmail.com>
2020-03-02 08:30:45 +0700

    temp

--- src/InputHead.tsx
+++ src/InputHead.tsx
@@ -53,6 +53,7 @@ function InputHead({ action, addTodo, changeCheckAll }: IProps) {
         onChange={e => changeText(e.target.value)}
         onKeyDown={e => (e.keyCode === 13 ? keyDown(!key) : null)}
       />
+      {/* <button type="button">+ Add New Item</button> */}
     </div>
   );
 }
